1526ba850444788ffdfc0486a69fbaf74c79bec5
akash sharma <22026768+00kaku@users.noreply.github.com>
2021-02-12 19:20:36 +0530

    Update PokemonsContainer.js
    Added pagination

--- src/PokemonsContainer.js
+++ src/PokemonsContainer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useQuery } from "@apollo/react-hooks";
 import { GET_POKEMONS } from "./graphql/getPokemons";
 import Pokemon from "./Pokemon";
@@ -9,12 +9,58 @@ function PokemonsContainer() {
 		variables: { first: 151 },
 	});
 
+	const [totalPages, setTotalPages] = useState(10);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [pokemonPerPage, setPokemonsPerPage] = useState(16);
+
+	let indexOfLastPokemon = currentPage * pokemonPerPage;
+	let indexOfFirstPokemon = indexOfLastPokemon - pokemonPerPage;
+	let currentPokemons = pokemons.slice(
+		indexOfFirstPokemon,
+		indexOfLastPokemon
+	);
+
+	const handleNext = () => {
+		setCurrentPage(currentPage + 1);
+	};
+
+	const handlePrevious = () => {
+		setCurrentPage(currentPage - 1);
+	};
+
+	useEffect(() => {
+		if (window.screen.width < 480) {
+			setTotalPages(51);
+			setPokemonsPerPage(3);
+		}
+
+		if (window.screen.width < 860 && window.screen.width > 480) {
+			setTotalPages(19);
+			setPokemonsPerPage(8);
+		}
+	}, []);
+
 	return (
-		<div className="pokemons">
-			{pokemons &&
-				pokemons.map((pokemon) => (
-					<Pokemon key={pokemon.id} pokemon={pokemon} />
-				))}
+		<div>
+			<div className="pokemons__pagination">
+				<button disabled={currentPage == 1} onClick={handlePrevious}>
+					Previous
+				</button>
+
+				<h1> POKEDEX </h1>
+				<button
+					disabled={currentPage == totalPages}
+					onClick={handleNext}
+				>
+					Next
+				</button>
+			</div>
+			<div className="pokemons">
+				{pokemons &&
+					currentPokemons.map((pokemon) => (
+						<Pokemon key={pokemon.id} pokemon={pokemon} />
+					))}
+			</div>
 		</div>
 	);
 }
